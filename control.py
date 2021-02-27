e3b68a53365bee0e96f3c63d5a6c27cf2d9f61c4
zernovski <vazzahov@gmail.com>
2021-02-27 18:27:39 +0600

    control task

---
+++ control.py
@@ -0,0 +1,43 @@
+#1
+lst = [92, 91, 49, 87, 74, 20, 94, 12, 64, 36, 97, 2, 96, 40, 97, 36, 32, 22, 80, 83, 49, 52, 62, 31,
+55, 86, 84, 1, 22, 15, 52, 18, 78, 92, 21, 9, 85, 89, 54, 99, 80, 7, 4, 31, 30, 28, 59, 35, 72, 33]
+
+dic = {index: i for index, i in enumerate(lst)}
+print(dic)
+
+
+#2
+import random
+a = random.randint(1, 20)
+
+
+i = 0
+
+while i < 5:
+
+    b = int(input("Введите число от 1 до 20:"))
+
+    if b < a:
+        print("Слишком мало!")
+    elif b > a:
+        print("Слишком много!")
+    elif b == a:
+        print("Класс вы угадали!")
+        break
+    i += 1
+
+if i == 5:
+    print("Все вы не угадали. Конец игры.")
+
+
+#3
+some_string = "Adverts"
+print(some_string[2:5])
+
+#4
+n = "Aidana"
+m = "Adilet"
+
+for i in range(len(n)) and range(len(m)):
+    s = n[i] + m[i]
+    print(s)
